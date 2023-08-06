8a0a53cb68818eed3b41f2c9e863a1f48cc710b4
Mikhail Ivanov <m.ivanov0427@gmail.com>
2023-08-06 18:48:17 +0300

    feat: add dotenv

--- src/consts.py
+++ src/consts.py
@@ -1,22 +1,14 @@
 import os
 
+from dotenv import load_dotenv
 
-def load_env(path):
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.strip("\n").split("=", 1)
+load_dotenv()
 
-            key = line[0]
-            value = line[1]
-            os.environ[key] = value
-
-
-load_env('.env')
-
-MON_BASE_URL = os.environ.get('MON_BASE_URL')
-MON_AUTH_LOGIN = os.environ.get('MON_AUTH_LOGIN')
-MON_AUTH_PASSWORD = os.environ.get('MON_AUTH_PASSWORD')
+MON_BASE_URL = os.getenv('MON_BASE_URL')
+MON_AUTH_LOGIN = os.getenv('MON_AUTH_LOGIN')
+MON_AUTH_PASSWORD = os.getenv('MON_AUTH_PASSWORD')
 MON_AUTH = (MON_AUTH_LOGIN, MON_AUTH_PASSWORD)
-GRAFANA_API_BASE_URL = os.environ.get('GRAFANA_API_BASE_URL')
-GRAFANA_API_TOKEN = os.environ.get('GRAFANA_API_TOKEN')
+GRAFANA_API_BASE_URL = os.getenv('GRAFANA_API_BASE_URL')
+GRAFANA_API_TOKEN = os.getenv('GRAFANA_API_TOKEN')
+
+
